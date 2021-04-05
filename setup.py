44a3e5f2f60f23c2c22de8be3c6a6746e5f72e5e
Phil <dreizen@kupad.net>
2021-04-05 19:41:58 +0000

    make sure whoosh is in deps

--- setup.py
+++ setup.py
@@ -25,10 +25,11 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    setup_requires=['wheel'],
     install_requires=[
-        'wheel',
         'flask',
         'moviepy',
-        'srt'
+        'srt',
+        'whoosh',
     ],
 )
