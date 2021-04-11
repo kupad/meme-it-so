66e59f2d5c41f9dcda3945fd98a90b6f6aef9a0d
kupad <dreizen@kupad.net>
2021-04-11 18:13:19 -0400

    tool for printing files that represent frames modulo 6

--- src/cli/find_nth_frame.py
+++ src/cli/find_nth_frame.py
@@ -1,17 +1,12 @@
-#quick tool for finding the nthframe in a directory of thumbs
-
-
 import os
 import sys
 
 nthframe=6
 dir = sys.argv[1]
 
-paths = []
 for dirpath, dirnames, filenames in os.walk(dir):
     for fname in filenames:
-        filepath = os.path.join(dirpath,fname)
         frame = int(fname.replace(".jpg",""))
         if frame % nthframe == 0:
+            filepath = os.path.join(dirpath.replace(dir,''),fname)
             print(filepath)
-            #paths.append( (frame, filepath))
