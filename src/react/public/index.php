4c214a305d49115bfdaa7bde47f931804a13cb20
kupad <dreizen@kupad.net>
2021-04-15 01:03:11 -0400

    new attempt for php replacements...

--- src/react/public/index.php
+++ src/react/public/index.php
@@ -2,12 +2,20 @@
 //point a server like nginx to this PHP instead of index.html and it can do substitutions before delivering the index.html content
 //good for  meta-tags that really change (for social media links / seo) even though we're delivering a SPA
 
-//right now just: 
-//PHP_HOOK gets replaced with the contents in phphook.txt. This isn't really a very efficient way to do the replace, since 
-//it's done on every request. sed would do nicely for this.
-$hookid = '<!-- PHP_HOOK  -->';
-$replace= file_get_contents('phphook.txt');
+//full html
 $html = file_get_contents('index.html');
-echo str_replace($hookid, $replace, $html);
+
+//phphook gets replaced with the contents in phphook.txt. This isn't really a very efficient way to do the replace, since
+//it's done on every server-side app request. sed would do nicely for this.
+$phphookid = '<script class="PHP_HOOK"></script>';
+$phphook_replace= file_get_contents('phphook.txt');
+$html = str_replace($phphookid, $phphook_replace, $html);
+
+$socialimagehook= '__SOCIAL_IMAGE__';
+$socialimage_replace = file_get_contents('socialimage.txt');
+$html = str_replace($socialimagehook, $socialimage_replace, $html);
+
+//echo out the modified html
+echo $html;
 
 ?>
